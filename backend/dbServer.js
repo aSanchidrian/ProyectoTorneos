6ed213b171044c4731ae76e49a99dbd08854446a
alexfp2001 <alejandro.perera@live.u-tad.com>
2023-03-27 14:28:59 +0200

    Update dbServer.js

--- backend/dbServer.js
+++ backend/dbServer.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use("/api", require("./routes")) 
 
 const port = process.env.PORT
-app.listen(port, 
+app.listen(port,  
 ()=> console.log(`Server Started on port ${port}...`))
 
 
