cfd5a1e2a9c0be1b50b1774e4a3e6f5729fbc4af
aSanchidrian <sanchimon.alex@gmail.com>
2023-03-30 21:36:22 +0200

    pagina principal y home en react

--- frontend/pagina_torneos/src/App.js
+++ frontend/pagina_torneos/src/App.js
@@ -1,46 +1,101 @@
 import React from "react";
 import "./App.css";
 import utad_logo from "./img.swapcard.png";
+import usports_logo from "./U-sports_logo.png";
+import web_icon from "./web-globe-icon-23.png";
 // import axios from "axios";
 // import { useState, useEffect } from "react";
+import "@fontsource/montserrat";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
   return (
     <div className="App">
-      <Navbar className="navbar navbar-expand-lg navbar-light bg-white">
-        <img className="ml-5" src={utad_logo} width="10%" height="10%" />
+      <Navbar>
+        <img
+          className="mr-4 responsive-image"
+          src={utad_logo}
+          style={{ maxWidth: "200px", maxHeight: "100px", margin: "auto" }}
+        />
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+          {/* <Navbar.Brand href="#home">Navbar with text</Navbar.Brand> */}
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text className="mr-3">
+              <Nav.Link href="#link" style={{ color: "#0066ef" }}>
+                Iniciar sesion
+              </Nav.Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <div class="collapse navbar-collapse" id="navbar-list-4">
+                <ul class="navbar-nav">
+                  <li class="nav-item dropdown">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdownMenuLink"
+                      role="button"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <img
+                        src={web_icon}
+                        width="40"
+                        height="40"
+                        class="rounded-circle"
+                      />
+                    </a>
+                    <div
+                      class="dropdown-menu"
+                      aria-labelledby="navbarDropdownMenuLink"
+                    >
+                      <a class="dropdown-item" href="#">
+                        Ingles
+                      </a>
+                      <a class="dropdown-item" href="#">
+                        España
+                      </a>
+                      <a class="dropdown-item" href="#">
+                        Aleman
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="d-flex justify-content-center">
-        <hr/>
+      {/* mitad de la pagina */}
+      <div className="d-flex justify-content-center mb-5">
+        <hr />
+      </div>
+      <br></br>
+      <div className="mt-5 d-flez justify-content-start">
+        <div className="display-4">
+          <p
+            className="mr-auto"
+            style={{ color: "#0066ef", fontFamily: "montserrat" }}
+          >
+            <b>Bienvenido a</b>
+          </p>
+          <img
+            className="responsive-image"
+            src={usports_logo}
+            style={{ maxWidth: "100%", maxHeight: "100%" }}
+          />
+        </div>
+        <br></br>
+        <div className="d-block gap-2">
+          <Button variant="primary" style={{borderRadius: "20px", width: ""}}>¡Comienza!</Button>
+        </div>
       </div>
       <footer className="d-flex justify-content-center">
-        <hr/>
+        <hr />
       </footer>
     </div>
   );
