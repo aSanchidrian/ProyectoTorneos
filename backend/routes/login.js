42c37013ce4effdf62ecbad87a805cd36b8af8ad
alexfp2001 <alejandro.perera@live.u-tad.com>
2023-03-31 12:11:18 +0200

    Delete login.js

--- backend/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router=express.Router()
-
-const {login}=require("../controllers/Users")
-require("dotenv").config()
-
-router.post("/",login) 
-
-    module.exports=router;
