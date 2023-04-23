50ca5c5f05f483b99fd70800214a8d4ff3abe30c
alexfp2001 <alejandro.perera@live.u-tad.com>
2023-04-23 13:51:45 +0200

    CHECk rol for admin

---
+++ backend/middleware/rol.js
@@ -0,0 +1,21 @@
+const { handleHttpError } = require("../utils/handleError")
+/**
+ * Array con los roles permitidos
+ * @param {*} roles
+ * @returns 
+ */
+
+const checkAdminRole = (req, res, next) => {
+    // Comprueba que req.user esté definido y que tenga una propiedad 'role'
+    if (req.user.role==1) {
+      // Retorna el rol del usuario
+      next()
+    }else{
+  
+    // Si no se encuentra un rol válido, retornamos un error de HTTP
+    handleHttpError(res, "NOT_PERMISSION_ONLY_FOR_ADMIN", 401);
+    }
+  };
+
+
+  module.exports = checkUserRole
